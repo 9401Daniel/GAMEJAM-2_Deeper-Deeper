8f0b35e7880d7f2fba84b2c2d8f02a46a42d4eed
SamuraiNyak <jhortab99@gmail.com>
2026-07-29 12:10:35 -0600

    Se agrego escena de prueba enemigos
    Carpeta de Scripts
    Carpeta de Prefabs
    Scripts de Eel, Mine, SeaUrchin, EnemyBase , EnemySpawner
    Scene = EnemigosJorge

---
+++ Assets/Scripts/Mine.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+public class Mine : EnemyBase
+{
+    // Por ahora, hereda todo el comportamiento de EnemyBase sin cambios.
+    // En el futuro, aquí podrías añadir lógica de explosiones o daño de área.
+}
---
+++ Assets/Scripts/SeaUrchin.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+public class SeaUrchin : EnemyBase
+{
+    // Al igual que los demás, la dirección (hacia abajo) se la dará el Spawner.
+    // Aquí podrías agregar rotación en el Update() en el futuro para que parezca que cae girando.
+}
